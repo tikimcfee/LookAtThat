6c15d938e28be40a28841e954da0060d9a933e7b
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-05-06 12:38:46 -0700

    - Cache built semantic info

--- Interop/Structures/SemanticInfoBuilder.swift
+++ Interop/Structures/SemanticInfoBuilder.swift
@@ -9,110 +9,147 @@ import Foundation
 import SwiftSyntax
 
 class SemanticInfoBuilder {
-	private let localSemanticCache = SyntaxCache()
+    private let localSemanticCache = SyntaxCache()
     private(set) var localCallStackCache = [String: Set<SemanticInfo>]()
+    private(set) var localInfoCache = [Syntax: SemanticInfo]()
     
     func semanticInfo(for node: Syntax, fileName: String? = nil) -> SemanticInfo {
-		switch self[node] {
+        if let cached = localInfoCache[node] { return cached }
+        
+        let info: SemanticInfo
+        switch self[node] {
         case .variableDecl(let varl):
-            var name: String
-            var stackName: String?
-            if let firstBinding = varl.bindings.first {
-                let typeName = firstBinding.typeAnnotation?.description ?? ""
-                let pattern = firstBinding.pattern.description
-                stackName = pattern.trimmingCharacters(in: .whitespaces)
-                name = "\(varl.letOrVarKeyword.text) \(pattern)\(typeName)"
-            } else {
-                name = "(unsupported syntax: \(varl.id)"
-            }
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: name,
-                color: CodeGridColors.variableDecl,
-                fullTextSearchable: true,
-                fileName: fileName,
-                callStackName: stackName
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeVariableInfo(for: node, fileName: fileName, varl)
             
         case .extensionDecl(let extenl):
-            let extendedTypeName = extenl.extendedType._syntaxNode.strippedText
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: "extension \(extendedTypeName)",
-                color: CodeGridColors.extensionDecl,
-                fileName: fileName,
-                callStackName: extendedTypeName.trimmingCharacters(in: .whitespaces)
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeExtensionInfo(for: node, fileName: fileName, extenl)
             
         case .classDecl(let classl):
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: "\(classl.identifier)",
-                color: CodeGridColors.classDecl,
-                fileName: fileName,
-                callStackName: "\(classl.identifier)".trimmingCharacters(in: .whitespaces)
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeClassInfo(for: node, fileName: fileName, classl)
             
         case .structDecl(let structl):
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: "\(structl.identifier)",
-                color: CodeGridColors.structDecl,
-                fileName: fileName,
-                callStackName: "\(structl.identifier)".trimmingCharacters(in: .whitespaces)
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeStructInfo(for: node, fileName: fileName, structl)
             
         case .functionDecl(let funcl):
-            let readableFunctionSignataure = "\(funcl.identifier)\(funcl.signature._syntaxNode.strippedText)"
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: readableFunctionSignataure,
-                color: CodeGridColors.functionDecl,
-                fileName: fileName,
-                callStackName: "\(funcl.identifier)".trimmingCharacters(in: .whitespaces)
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeFunctionInfo(for: node, fileName: fileName, funcl)
             
         case .protocolDecl(let protol):
-            let newInfo = SemanticInfo(
-                node: node,
-                referenceName: "\(protol.identifier)",
-                color: CodeGridColors.protocolDecl,
-                fileName: fileName,
-                callStackName: "\(protol.identifier)".trimmingCharacters(in: .whitespaces)
-            )
-            addToCallStackCache(newInfo)
-            return newInfo
+            info = makeProtocolInfo(for: node, fileName: fileName, protol)
             
         default:
-            return SemanticInfo(
-                node: node,
-                fileName: fileName
-            )
-		}
-	}
+            info = makeDefaultInfo(for: node, fileName : fileName)
+        }
+        
+        localInfoCache[node] = info
+        return info
+    }
+}
+
+extension SemanticInfoBuilder {
+    private func addToCallStackCache(_ info: SemanticInfo) {
+        guard !info.callStackName.isEmpty else { return }
+        localCallStackCache[info.callStackName, default: []].insert(info)
+    }
     
     public func cachedCallStackInfo(_ callStackName: String) -> Set<SemanticInfo>? {
         localCallStackCache[callStackName]
     }
-	
-	subscript(_ node: Syntax) -> SyntaxEnum {
-		get { localSemanticCache[node].nodeEnum }
-	}
+    
+    subscript(_ node: Syntax) -> SyntaxEnum {
+        get { localSemanticCache[node].nodeEnum }
+    }
 }
 
 private extension SemanticInfoBuilder {
-    func addToCallStackCache(_ info: SemanticInfo) {
-        guard !info.callStackName.isEmpty else { return }
-        localCallStackCache[info.callStackName, default: []].insert(info)
+    func makeVariableInfo(for node: Syntax, fileName: String? = nil, _ varl: VariableDeclSyntax) -> SemanticInfo {
+        var name: String
+        var stackName: String?
+        if let firstBinding = varl.bindings.first {
+            let typeName = firstBinding.typeAnnotation?.description ?? ""
+            let pattern = firstBinding.pattern.description
+            stackName = pattern.trimmingCharacters(in: .whitespaces)
+            name = "\(varl.letOrVarKeyword.text) \(pattern)\(typeName)"
+        } else {
+            name = "(unsupported syntax: \(varl.id)"
+        }
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: name,
+            color: CodeGridColors.variableDecl,
+            fullTextSearchable: true,
+            fileName: fileName,
+            callStackName: stackName
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeExtensionInfo(for node: Syntax, fileName: String? = nil, _ extenl: ExtensionDeclSyntax) -> SemanticInfo {
+        let extendedTypeName = extenl.extendedType._syntaxNode.strippedText
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: "extension \(extendedTypeName)",
+            color: CodeGridColors.extensionDecl,
+            fileName: fileName,
+            callStackName: extendedTypeName.trimmingCharacters(in: .whitespaces)
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeClassInfo(for node: Syntax, fileName: String? = nil, _ classl: ClassDeclSyntax) -> SemanticInfo {
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: "\(classl.identifier)",
+            color: CodeGridColors.classDecl,
+            fileName: fileName,
+            callStackName: "\(classl.identifier)".trimmingCharacters(in: .whitespaces)
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeStructInfo(for node: Syntax, fileName: String? = nil, _ structl: StructDeclSyntax) -> SemanticInfo {
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: "\(structl.identifier)",
+            color: CodeGridColors.structDecl,
+            fileName: fileName,
+            callStackName: "\(structl.identifier)".trimmingCharacters(in: .whitespaces)
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeFunctionInfo(for node: Syntax, fileName: String? = nil, _ funcl: FunctionDeclSyntax) -> SemanticInfo {
+        let readableFunctionSignataure = "\(funcl.identifier)\(funcl.signature._syntaxNode.strippedText)"
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: readableFunctionSignataure,
+            color: CodeGridColors.functionDecl,
+            fileName: fileName,
+            callStackName: "\(funcl.identifier)".trimmingCharacters(in: .whitespaces)
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeProtocolInfo(for node: Syntax, fileName: String? = nil, _ protol: ProtocolDeclSyntax) -> SemanticInfo {
+        let newInfo = SemanticInfo(
+            node: node,
+            referenceName: "\(protol.identifier)",
+            color: CodeGridColors.protocolDecl,
+            fileName: fileName,
+            callStackName: "\(protol.identifier)".trimmingCharacters(in: .whitespaces)
+        )
+        addToCallStackCache(newInfo)
+        return newInfo
+    }
+    
+    func makeDefaultInfo(for node: Syntax, fileName: String? = nil) -> SemanticInfo {
+        return SemanticInfo(
+            node: node,
+            fileName: fileName
+        )
     }
 }
