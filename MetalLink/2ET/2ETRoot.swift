ff376cf0cf24bfc67717f9b0fada22006c5406e7
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-08-09 22:25:27 -0700

    - Spin some cubes

--- MetalLink/2ET/2ETRoot.swift
+++ MetalLink/2ET/2ETRoot.swift
@@ -18,7 +18,7 @@ class TwoETimeRoot: MetalLinkReader {
     
     init(link: MetalLink) throws {
         self.link = link
-        try setup2()
+        try setup3()
     }
     
     func delegatedEncode(in sdp: inout SafeDrawPass) {
@@ -28,6 +28,29 @@ class TwoETimeRoot: MetalLinkReader {
 }
 
 extension TwoETimeRoot {
+    func setup3() throws {
+        view.clearColor = MTLClearColorMake(0.03, 0.1, 0.2, 1.0)
+        
+        let (min, max) = (-3, 3)
+        let length = (min..<max)
+        let count = Float(length.count)
+        let halfCount = count / 2.0 // iterate from left to right, left/right symmetry
+        
+        for x in length {
+            let xPos = Float(x) + 0.5
+            for y in length {
+                let yPos = Float(y) + 0.5
+                for z in length {
+                    let zPos = Float(z) + 0.5
+                    let cube = try CubeNode(link: link)
+                    cube.position = LFloat3(xPos / halfCount, yPos / halfCount, zPos / halfCount)
+                    cube.scale = LFloat3(repeating: 1 / (count + 10))
+                    root.add(child: cube)
+                }
+            }
+        }
+    }
+    
     func setup2() throws {
         view.clearColor = MTLClearColorMake(0.03, 0.1, 0.2, 1.0)
         
--- MetalLink/MetalLinkObjects/ObjectTypes/CubeNode.swift
+++ MetalLink/MetalLinkObjects/ObjectTypes/CubeNode.swift
@@ -7,8 +7,19 @@
 
 import MetalKit
 
+func randomFloat(_ range: Range<Float> = (0..<1)) -> Float { Float.random(in: range) }
+
 class CubeNode: MetalLinkObject {
     init(link: MetalLink) throws {
         try super.init(link, mesh: link.meshes[.Cube])
+    }
+    
+    private var _time: Float = 0
+    private func time(_ dT: Float) -> Float { _time += dT; return _time }
+    override func update(deltaTime dT: Float) {
+        rotation = LFloat3(cos(time(dT)),
+                           sin(time(dT)),
+                           1)
+        super.update(deltaTime: dT)
     }
 }
