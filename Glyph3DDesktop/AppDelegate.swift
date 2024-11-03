7e52de1c547d5c803856c9f97359d16a09a1910b
Ivan Lugo <ivan.lugo.dev@pm.me>
2024-11-03 13:37:46 -0600

    - Just make another project because whatever. Again.

---
+++ Glyph3DDesktop/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Glyph3DDesktop
+//
+//  Created by Ivan Lugo on 11/3/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Glyph3DDesktop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Glyph3DDesktop
+//
+//  Created by Ivan Lugo on 11/3/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
