43cf3991252a31647532a61d8d90c40cbdee7078
Ivan Lugo <ivan.lugo.dev@pm.me>
2024-11-03 13:33:01 -0600

    Initial Commit

---
+++ Glyph3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glyph3D
+//
+//  Created by Ivan Lugo on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glyph3D/Glyph3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glyph3DApp.swift
+//  Glyph3D
+//
+//  Created by Ivan Lugo on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Glyph3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
